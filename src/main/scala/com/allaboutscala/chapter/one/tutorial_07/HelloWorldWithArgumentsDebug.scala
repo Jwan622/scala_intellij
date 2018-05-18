e70ffcdf814f996fd87494743a2f195eb6df9c14
Jwan622 <jeffrey.wan@gemini.com>
2018-05-18 18:16:47 -0400

    first commit

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_07/HelloWorldWithArgumentsDebug.scala
@@ -0,0 +1,7 @@
+package com.allaboutscala.chapter.one.tutorial_07
+
+object HelloWorldWithArgumentsDebug extends App {
+	println("hello world with arguments scala application")
+	println("command line arguments are: ")
+	println(args.mkString(", "))
+}

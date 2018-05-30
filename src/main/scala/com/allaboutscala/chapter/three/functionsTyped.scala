4ac94f2b37d199283eb26ba0d733e1b9e44b495c
Jwan622 <jeffrey.wan@gemini.com>
2018-05-30 09:51:00 -0400

    more scala

---
+++ src/main/scala/com/allaboutscala/chapter/three/functionsTyped.scala
@@ -0,0 +1,37 @@
+package com.allaboutscala.chapter.three
+
+object functionsTyped extends App {
+	println("Step 1: How to define a function which takes a String parameter")
+	def applyDiscount(couponCode: String) {
+		println(s"Lookup percentage discount in database for $couponCode")
+	}
+
+	println("\nStep 2: How to define a function which takes a parameter of type Double")
+	def applyDiscount(percentageDiscount: Double) {
+		println(s"$percentageDiscount discount will be applied")
+	}
+
+	println("\nStep 3: Calling applyDiscount function with String or Double parameter types")
+	applyDiscount("COUPON_1234")
+	applyDiscount(10)
+
+	// create typed functions which will allow you to specify the types of the parameters when calling the function.
+	println("\nStep 4: How to define a generic typed function which will specify the type of its parameter")
+	def applyDiscount[T](discount: T) {
+		discount match {
+			case d: String =>
+				println(s"Lookup percentage discount in database for $d")
+
+			case d: Double =>
+				println(s"$d discount will be applied")
+
+			case _ =>
+				println("Unsupported discount type")
+		}
+	}
+
+	println("\nStep 5: How to call a function which has typed parameters")
+	applyDiscount[String]("COUPON_123")
+	applyDiscount[Double](discount = 10)
+
+}
